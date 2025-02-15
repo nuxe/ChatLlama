e80c9306708849c2399c8d9cae310d9b0ea04d5b
kush <kush2005@gmail.com>
2025-02-14 21:44:46 -0500

    Initial Commit

---
+++ ChatLlamaUITests/ChatLlamaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ChatLlamaUITestsLaunchTests.swift
+//  ChatLlamaUITests
+//
+//  Created by Kush Agrawal on 2/14/25.
+//
+
+import XCTest
+
+final class ChatLlamaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
